453e68741fe5aec6b8dd168e8ba04aabc29eb217
unknown <tawfikalhumairi@gmail.com>
2025-03-21 12:13:18 +0300

    week5

---
+++ week5/src/point.java
@@ -0,0 +1,15 @@
+public class point {
+
+    public int xcoord,ycoord;
+
+    public point (){
+    }
+    public point (int x,int y){
+        this .xcoord=x;
+        this.ycoord=y;
+    }
+    public point (int x){
+        this.xcoord=x;
+        this.ycoord=y;
+    }
+}
